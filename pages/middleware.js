3f41836700b5357af7d7c1a76cc2ebc4a129c2f0
Mrudul Patel <mrudulpatel04@gmail.com>
2023-01-12 12:31:54 +0530

    removed middleware

--- pages/middleware.js
+++ pages/middleware.js
@@ -1,17 +1,17 @@
-import { getToken } from "next-auth/jwt";
-import { NextResponse } from "next/server";
+// import { getToken } from "next-auth/jwt";
+// import { NextResponse } from "next/server";
 
-export async function middleware(req) {
-  const token = await getToken({ req, secret: process.env.JWT_SECRET });
-  const { pathname } = req.nextUrl;
-  //Allow the req if the following is true
-  //   1. IF token exists
-  // 2. Token exists
-  if (pathname.includes("/api/auth") || token) {
-    return NextResponse.next();
-  }
+// export async function middleware(req) {
+//   const token = await getToken({ req, secret: process.env.JWT_SECRET });
+//   const { pathname } = req.nextUrl;
+//   //Allow the req if the following is true
+//   //   1. IF token exists
+//   // 2. Token exists
+//   if (pathname.includes("/api/auth") || token) {
+//     return NextResponse.next();
+//   }
 
-  if (!token && pathname != "/login") {
-    return NextResponse.redirect("/login");
-  }
-}
+//   if (!token && pathname != "/login") {
+//     return NextResponse.redirect("/login");
+//   }
+// }
